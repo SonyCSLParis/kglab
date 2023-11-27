47992066d75075919aad4e105a569a5679f694f7
Ines Blin <iblin431@outlook.fr>
2023-11-27 14:38:09 +0100

    updating graph comparison

--- kglab/helpers/graph_structure.py
+++ kglab/helpers/graph_structure.py
@@ -3,13 +3,24 @@
 Generic helpers with graphs (mostly rdf_ones)
 """
 from rdflib import Graph
+from urllib.parse import unquote
+
+def get_unquoted_triples(graph):
+    res = []
+    for subject, predicate, obj in graph:
+        res.append((unquote(str(subject)), unquote(str(predicate)), unquote(str(obj))))
+    return res
 
 def get_intersection_difference(g1: Graph, g2: Graph) -> (list[tuple], Graph, Graph):
     """ returns (intersection, g1-g2, g2-g1) """
+    g1 = get_unquoted_triples(graph=g1)
+    g2 = get_unquoted_triples(graph=g2)
+
     intersection = []
     smaller = g1 if len(g1) < len(g2) else g2
     bigger = g2 if len(g1) < len(g2) else g1
+
     for triple in smaller:
         if triple in bigger:
             intersection.append(triple)
-    return intersection, g1-g2, g2-g1
+    return intersection, [x for x in g1 if x not in g2], [x for x in g2 if x not in g1]
